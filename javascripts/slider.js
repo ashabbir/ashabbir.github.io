39a9cc6e04ad4ec14ba2760571c8441cca103b73
Ahmed Shabbir <shabbir10314@gmail.com>
2013-07-16 17:20:12 -0400

    just added charts in slider

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -1,10 +1,9 @@
- $(document).ready(function() {
-            
+ $(function() {
             var itemWidth = $("#container li").width();
             // hide 50% of each window
-            var itemPosition = itemWidth * 50 / 100;
+            var itemPosition = itemWidth * 20 / 100;
             // slide each window 60% if its width    
-            var itemMove = itemWidth * 60 / 100;        
+            var itemMove = itemWidth * 65 / 100;        
 
             // move windows below eachother
             $("#container li").each(function(i) {
@@ -50,4 +49,35 @@
                 }
             });
         });
+$(function() {
 
+		var data = [ ["January", 10], ["February", 8], ["March", 4], ["April", 13], ["May", 17], ["June", 9] ];
+
+		$.plot("#placeholder1", [ data ], {
+			series: {
+				bars: {
+					show: true,
+					barWidth: 0.6,
+					align: "center"
+				}
+			},
+			xaxis: {
+				mode: "categories",
+				tickLength: 0
+			}
+		});
+  $.plot("#placeholder2", [ data ], {
+			series: {
+				bars: {
+					show: true,
+					barWidth: 0.6,
+					align: "center"
+				}
+			},
+			xaxis: {
+				mode: "categories",
+				tickLength: 0
+			}
+		});
+
+	});
